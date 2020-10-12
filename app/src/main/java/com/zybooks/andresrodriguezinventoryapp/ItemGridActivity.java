c61f5f286bd02ddbb0fdccc1a7d53b9d86faa980
Andres Rodriguez <andres6459@gmail.com>
2020-10-12 10:31:57 -0700

    Initial Commit

---
+++ app/src/main/java/com/zybooks/andresrodriguezinventoryapp/ItemGridActivity.java
@@ -0,0 +1,14 @@
+package com.zybooks.andresrodriguezinventoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_grid);
+    }
+}
---
+++ app/src/main/java/com/zybooks/andresrodriguezinventoryapp/NotificationPermissionActivity.java
@@ -0,0 +1,14 @@
+package com.zybooks.andresrodriguezinventoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationPermissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_permission);
+    }
+}
