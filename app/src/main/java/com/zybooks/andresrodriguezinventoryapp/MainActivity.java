a589ec8f67cfc6a3a9b4c22f7929d9ca05dd44bb
Andres Rodriguez <andresrodriguez64@yahoo.com>
2020-10-12 16:02:30 -0700

    Login button reads username and password when tapped.

--- app/src/main/java/com/zybooks/andresrodriguezinventoryapp/MainActivity.java
+++ app/src/main/java/com/zybooks/andresrodriguezinventoryapp/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextUsername;
     private EditText editTextPassword;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+    }
+
+    public void tapLogin(View view) {
+        System.out.println("Login button has been tapped!");
+
+        String username = editTextUsername.getText().toString();
+        String userPassword = editTextPassword.getText().toString();
+
+        System.out.println("Username: " + username + "\nPassword: " + userPassword);
     }
 
     @Override
