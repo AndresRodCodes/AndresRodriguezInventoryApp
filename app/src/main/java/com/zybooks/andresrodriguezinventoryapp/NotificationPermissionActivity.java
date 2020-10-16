7c4201f2d9365889faafd632fd1bf94951702611
Andres Rodriguez <andres6459@gmail.com>
2020-10-16 13:45:18 -0700

    Completed Requesting SMS permission when the request permission button is tapped.

--- app/src/main/java/com/zybooks/andresrodriguezinventoryapp/NotificationPermissionActivity.java
+++ app/src/main/java/com/zybooks/andresrodriguezinventoryapp/NotificationPermissionActivity.java
@@ -1,19 +1,71 @@
 package com.zybooks.andresrodriguezinventoryapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class NotificationPermissionActivity extends AppCompatActivity {
+    private final int SMS_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_permission);
+
+        // TODO: Update permission status text to granted if it has been granted
     }
 
     public void onTapRequestPermission(View view) {
         System.out.println("Request Permission button tapped.");
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+        } else {
+            requestSendSMSPermission();
+        }
+    }
+
+    private void requestSendSMSPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is need to send SMS text when inventory is low.")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(NotificationPermissionActivity.this, new String[] {Manifest.permission.SEND_SMS}, SMS_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS}, SMS_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == SMS_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
